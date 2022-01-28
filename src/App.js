d0a119791f9223ddd7b59320e394378b88434c00
Calum <calum.rogers@ntlworld.com>
2022-01-28 14:41:10 +0000

    first commit connected to yahoo finance API through web socket price data showing on page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react'
+import protobuf from 'protobufjs'
+const { Buffer } = require('buffer/')
+
+
+function formatPrice(price){
+  return `£${price.toFixed(2)}`;
+}
 
 function App() {
+
+  const[stock, setStock] = useState(null)
+
+  useEffect(() => {
+    // connects to yahoo api
+    const ws = new WebSocket('wss://streamer.finance.yahoo.com');
+    const root = protobuf.load('./YPricingData.proto', (error, root) => {
+      if (error) {
+        return console.log(error);
+      }
+
+
+      const Yaticker = root.lookupType("yaticker");
+
+      ws.onopen = function open() {
+        console.log('connected');
+        ws.send(JSON.stringify({
+          subscribe: ['GME']
+        }));
+      };
+
+      ws.onclose = function close() {
+        console.log('disconnected');
+      };
+
+      ws.onmessage = function incoming(message) {
+        // message.data
+        const next = Yaticker.decode(new Buffer(message.data, 'base64'));
+        setStock(next)
+      };
+    });
+
+
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+      <h1>Stocks</h1>
+      {stock && <h2>{formatPrice(stock.price)}</h2>}
+      
     </div>
   );
 }
